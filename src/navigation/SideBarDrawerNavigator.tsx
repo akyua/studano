913d3abd8d7bed484a572cc7753316d42cd5411a
tcatarina <tcatarina@proton.me>
2025-05-13 23:00:13 -0300

    [TDEV-2] Adicionado novos menus na sidebar.

--- src/navigation/SideBarDrawerNavigator.tsx
+++ src/navigation/SideBarDrawerNavigator.tsx
@@ -5,7 +5,16 @@ import SideBar from "@/components/SideBar";
 import { SideBarDrawerElements } from './types';
 import { HomeStackNavigator } from './HomeStackNavigator';
 import Subjects from "@/screens/Subjects"
+import Schedules from "@/screens/Schedules"
+import History from "@/screens/History"
 import { useTranslation } from 'react-i18next';
+import {
+  Home as HomeIcon,
+  Settings as SettingsIcon,
+  Book as BookIcon,
+  Clock as ClockIcon,
+  History as HistoryIcon
+} from "lucide-react-native"
 
 const Drawer = createDrawerNavigator<SideBarDrawerElements>();
 
@@ -29,6 +38,9 @@ export function SideBarDrawerNavigator() {
         options={{
           title: t("home.title"),
           headerShown: true,
+          drawerIcon: ({ color, size }) => {
+            return <HomeIcon color={color} size={size} />
+          }
         }}
       />
 
@@ -38,6 +50,9 @@ export function SideBarDrawerNavigator() {
         options={{
           title: t("settings.title"),
           headerShown: true,
+          drawerIcon: ({ color, size }) => {
+            return <SettingsIcon color={color} size={size} />
+          }
         }}
       />
 
@@ -47,6 +62,33 @@ export function SideBarDrawerNavigator() {
         options={{
           title: t("subjects.title"),
           headerShown: true,
+          drawerIcon: ({ color, size }) => {
+            return <BookIcon color={color} size={size} />
+          }
+        }}
+      />
+
+      <Drawer.Screen
+        name="Schedules"
+        component={Schedules}
+        options={{
+          title: t("schedules.title"),
+          headerShown: true,
+          drawerIcon: ({ color, size }) => {
+            return <ClockIcon color={color} size={size} />
+          }
+        }}
+      />
+
+      <Drawer.Screen
+        name="History"
+        component={History}
+        options={{
+          title: t("history.title"),
+          headerShown: true,
+          drawerIcon: ({ color, size }) => {
+            return <HistoryIcon color={color} size={size} />
+          }
         }}
       />
 
